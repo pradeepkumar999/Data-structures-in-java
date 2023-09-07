defd587b0d21f603e4064c73887b01ca5716de3b
pradeep <pradeepvirat96@gmail.com>
2023-09-07 16:16:17 +0530

    Stack al

---
+++ Stacks&queues/StackAL.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class StackAL {
+
+
+
+    static class Stack{
+
+    static ArrayList <Integer> list=new ArrayList<>();
+    
+    public static Boolean isEmpty(){
+        return list.size()==0;
+    }
+
+
+
+    public static void push(int data){
+        list.add(data);
+        return;
+    }
+
+    public static int pop(){
+        if(isEmpty()){
+            return -1;
+        }
+        int top=list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+        
+
+    } 
+
+    public static int peek(){
+        return list.get(list.size()-1);
+         
+    }
+}
+
+
+    public static void main(String args[]){
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+    
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+    
+    
+}
+    }
+}
