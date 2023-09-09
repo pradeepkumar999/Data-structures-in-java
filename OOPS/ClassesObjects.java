455086306262203571a10268325045da36d839f5
pradeep <pradeepvirat96@gmail.com>
2023-09-09 18:18:24 +0530

    oops

---
+++ OOPS/ClassesObjects.java
@@ -0,0 +1,11 @@
+
+
+public class ClassesObjects {
+    int x=50;
+    public static void main(String [] args){
+        //creating object my main class 
+        ClassesObjects myObj=new ClassesObjects();
+        //accessing the object through .
+        System.out.println(myObj.x);
+    }
+}
