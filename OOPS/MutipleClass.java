1cbcbb6586a6cac5dd341867057ce3cd4a169412
pradeep <pradeepvirat96@gmail.com>
2023-09-09 18:29:14 +0530

    classes

---
+++ OOPS/MutipleClass.java
@@ -0,0 +1,10 @@
+//created other class with name Main
+/*public class Main{
+  int x=15;
+}*/
+public class MutipleClass {
+    public static void main(String []args){
+        Main myObj=new Main();
+        System.out.println(myObj.x);
+    }
+}
