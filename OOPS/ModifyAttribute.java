528d474591df451db9140d1f4451157d75fb96ad
pradeep <pradeepvirat96@gmail.com>
2023-09-09 18:40:40 +0530

    attributes

---
+++ OOPS/ModifyAttribute.java
@@ -0,0 +1,11 @@
+
+
+public class ModifyAttribute {
+    int x=15;
+    public static void main(String args[]){
+    ModifyAttribute myObj=new ModifyAttribute();
+    myObj.x=40;
+    System.out.println(myObj.x);
+    }
+    
+}
