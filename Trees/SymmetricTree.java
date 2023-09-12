a75967410e7a41289817df97c867a93f56a95612
pradeep <pradeepvirat96@gmail.com>
2023-09-12 21:41:11 +0530

    leet code Symmetric tree

---
+++ Trees/SymmetricTree.java
@@ -0,0 +1,34 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class SymmetricTree{
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return isMirror(root.left,root.right);
+    }
+    public boolean isMirror(TreeNode left,TreeNode right){
+        if(left==null&&right==null){
+            return true;
+        }
+        if(left==null||right==null){
+            return false;
+        }
+
+        return (left.val==right.val)&&isMirror(left.left,right.right)&&isMirror(left.right,right.left);
+    }
+}
